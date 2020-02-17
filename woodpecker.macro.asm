a09a10fd235b6c85c7bf12b5527e36ca298498bd
gr8b <gr8b.github@gmail.com>
2020-02-18 00:04:17 +0200

    added sending byte index

--- woodpecker.macro.asm
+++ woodpecker.macro.asm
@@ -1,4 +1,5 @@
 .def	data_loop_register		=	r28
+.def	data_loop_index_register =	r27
 
 
 
@@ -13,8 +14,12 @@
 	ldi		ZL,low(@0*2)
 	ldi  	ZH,high(@0*2)
 	ldi		data_loop_register,@1
+	ldi		data_loop_index_register,1
 send_data_loop:
+	mov		byte_register,data_loop_index_register
+	rcall	bitsender
 	rcall	send_byte
+	inc		data_loop_index_register
 	dec		data_loop_register
 	brne	send_data_loop
 .endmacro
