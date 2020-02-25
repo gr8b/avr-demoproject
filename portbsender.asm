582241fdb51023bec2d3e701b2a581daf378cccd
gr8b <gr8b.github@gmail.com>
2020-02-25 17:54:02 +0200

    added C code as comments

--- portbsender.asm
+++ portbsender.asm
@@ -13,26 +13,33 @@
 
 
 write:
-	cbi		PORTB,cs
+	cbi		PORTB,cs				;	digitalWrite(MAX7219::cs, LOW);
 	mov		byte_register,command_register
-	rcall	write_byte
+	rcall	write_byte				;	put(command_register)
 	mov		byte_register,data_register
-	rcall	write_byte
-	sbi		PORTB,cs
-	cbi		PORTB,clk
+	rcall	write_byte				;	put(data_register)
+	sbi		PORTB,cs				;	digitalWrite(MAX7219::cs, HIGH);
+	cbi		PORTB,clk				;	digitalWrite(MAX7219::clk, LOW);
 	ret
 
-write_byte:
-	ldi		temp,8
-write_byte_loop:
-	cbi		PORTB,clk
-	lsl		byte_register
-	cbi		PORTB,din
-	brcc	write_byte_skip
-	sbi		PORTB,din
-write_byte_skip:
-	sbi		PORTB,clk
+write_byte:							; put(data)
+	ldi		temp,8					;	for(i = 0; i < 8; i++)
+write_byte_loop:					;	{
+	cbi		PORTB,clk				;		digitalWrite(MAX7219::clk, LOW);
+	lsl		byte_register			;		(data & 0x80) ? HIGH : LOW
+	brcs	write_high				;
+	cbi		PORTB,din				;		digitalWrite(MAX7219::din, LOW);
+	rjmp	write_low				;
+write_high:							;
+	sbi		PORTB,din				;		digitalWrite(MAX7219::din, HIGH);
+write_low:							;
+	sbi		PORTB,clk				;		digitalWrite(MAX7219::clk, HIGH);
 	dec		temp
-	brne	write_byte_loop
+
+	nop
+	nop
+	nop
+
+	brne	write_byte_loop			;	}
 	ret
 
